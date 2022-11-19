6164cbcdff627383362bfcee6310e519d0127fa0
Bilal <113285404+Bilal-Abykeev@users.noreply.github.com>
2022-11-19 10:23:08 +0300

    Add files via upload

---
+++ dz2/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import AboutUs from './pages/AboutUs';
+import Portfolio from './pages/Portfolio';
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
